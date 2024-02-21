3f5d2793db5a1328fe54bafb1a959a7ed71b7e89
muneebawan44 <143783326+muneebawan44@users.noreply.github.com>
2024-02-21 12:36:48 +0500

    Add files via upload

--- page.js
+++ page.js
@@ -1,26 +1,51 @@
+'use client'
+// import Detail from "./Detail";
 import Link from "next/link";
-import Image from "next/image";
+import { useEffect, useState } from 'react';
 
-export default async function Dashboard() {
-  const res = await fetch('https://api.imgflip.com/get_memes');
-  const data = await res.json();
-  const images = data.data.memes;
 
-  return (
-    
-    
-      <div  className="main-card-div" > 
-        {images.map((item, index) => (
-          <div key={index}  className="mb-0 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
-            <Link href={`/detail/${item.id}`} passHref>
-              <div className="border-2 p-2 w-60 h-auto m-3 inline-block" rel="noopener noreferrer">
-                <img src={item.url} alt={item.name} width={100} height={100} className="p-2 w-52 h-52 " />
-                <span className="image-name">{item.name}</span>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    
-  );
-}
+export default  function Details({params}){
+    const [selectedMemeUrl, setSelectedMemeUrl] = useState('');
+    const [text1, setText1] = useState('');
+    const [text2, setText2] = useState('');
+    const [generatedMemeUrl, setGeneratedMemeUrl] = useState('');
+
+    useEffect(() => {
+        fetchMeme();
+    }, []);
+
+    async function fetchMeme() {
+        const res = await fetch('https://api.imgflip.com/get_memes')
+        const result = await res.json()
+        const memes = result.data.memes.find(memes => memes.id === params.id);
+        setSelectedMemeUrl(memes.url)
+    }
+
+    async function generateMeme(){
+        const res = await fetch(` https://api.imgflip.com/caption_image?template_id=${params.id}&username=MuneebAhmed3&password=muneeb12&text0=${text1}&text1=${text2}`)
+        const result = await res.json()
+        if (result.success) {
+            setGeneratedMemeUrl(result.data.url);
+        } else {
+            alert(result.error_message);
+        }
+    } 
+
+
+
+    return <div> 
+         <img className='w-64 h-64 m-4' src={selectedMemeUrl} alt="Selected Meme" />
+         <div className='mb-4'>
+                <label htmlFor="text1" className='mr-2'>Text 1:</label>
+                <input className='h-12 p-2 border border-black rounded' type="text" id="text1" value={text1} onChange={(e) => setText1(e.target.value)} />
+            </div>
+            <div className='mb-4'>
+                <label htmlFor="text2" className='mr-2'>Text 2:</label>
+                <input className='h-12 p-2 border border-black rounded' type="text" id="text2" value={text2} onChange={(e) => setText2(e.target.value)} />
+            </div>
+            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={generateMeme}>Generate</button>
+            <div>
+                 {generatedMemeUrl && <img className='w-64 h-64' src={generatedMemeUrl} alt="Generated Meme" />}
+            </div>
+    </div>
+    }
